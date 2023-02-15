2adcdf6dc54837ec7cf9a2382638505632d74047
Artem Iliev <artemilev05@gmail.com>
2023-02-15 17:57:49 +0200

    First commit

--- src/Third.java
+++ src/Third.java
@@ -5,14 +5,14 @@ public class Third {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введіть кількість грибів");
         int kmushrooms = scanner.nextInt();
-        if ( kmushrooms > 4)
+        if (( kmushrooms % 10 == 0 ) || (kmushrooms > 4 && kmushrooms <= 20) || (kmushrooms % 10 >= 5))
         {
             System.out.println("Ви знайшли в лісі " + kmushrooms + " грибів.");
         }
-        else if (kmushrooms == 1) {
+        else if (kmushrooms % 10 == 1) {
             System.out.println("Ви знайшли в лісі " + kmushrooms + " гриб.");
         }
-        else if (kmushrooms == 2 || kmushrooms == 3 || kmushrooms == 4) {
+        else if ((kmushrooms % 10 == 2) || (kmushrooms % 10 == 3) || (kmushrooms % 10 == 4)) {
             System.out.println("Ви знайшли в лісі " + kmushrooms + " гриба.");
         } else if (kmushrooms == 0 ) {
             System.out.println("Ви не знайшли жодного гриба.");
